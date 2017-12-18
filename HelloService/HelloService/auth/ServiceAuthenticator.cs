a2b5cd7de7a688ad435f040c8996d2c798d24d8b
pankaj dhami <p.dhami@agilecockpit.com>
2017-12-18 18:29:55 +0530

    service changes

---
+++ HelloService/HelloService/auth/ServiceAuthenticator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IdentityModel.Selectors;
+using System.ServiceModel;
+
+namespace HelloService.auth
+{
+    class ServiceAuthenticator : UserNamePasswordValidator
+    {
+        public override void Validate(string userName, string password)
+        {
+            try
+            {
+                if (userName == "test" && password == "test123")
+                {
+                    Console.WriteLine("Authentic User");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new FaultException("Unknown Username or Incorrect Password");
+            }
+        }
+    }
+}
