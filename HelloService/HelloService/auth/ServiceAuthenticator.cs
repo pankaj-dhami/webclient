9c11263938b72291006f8055e7bfd53e39cb5d3a
pankaj dhami <p.dhami@agilecockpit.com>
2017-12-19 18:12:37 +0530

    changes

--- HelloService/HelloService/auth/ServiceAuthenticator.cs
+++ HelloService/HelloService/auth/ServiceAuthenticator.cs
@@ -5,10 +5,12 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IdentityModel.Selectors;
 using System.ServiceModel;
+using System.Security.Cryptography.X509Certificates;
+using System.IdentityModel.Tokens;
 
 namespace HelloService.auth
 {
-    class ServiceAuthenticator : UserNamePasswordValidator
+    public class ServiceAuthenticator : UserNamePasswordValidator
     {
         public override void Validate(string userName, string password)
         {
@@ -24,5 +26,36 @@ namespace HelloService.auth
                 throw new FaultException("Unknown Username or Incorrect Password");
             }
         }
+    }
+    public class MyX509CertificateValidator : X509CertificateValidator
+    {
+        string allowedIssuerName;
+
+        public MyX509CertificateValidator(string allowedIssuerName)
+        {
+            if (allowedIssuerName == null)
+            {
+                throw new ArgumentNullException("allowedIssuerName");
+            }
+
+            this.allowedIssuerName = allowedIssuerName;
+        }
+
+        public override void Validate(X509Certificate2 certificate)
+        {
+            // Check that there is a certificate.
+            if (certificate == null)
+            {
+                throw new ArgumentNullException("certificate");
+            }
+
+            // Check that the certificate issuer matches the configured issuer.
+            if (allowedIssuerName != certificate.IssuerName.Name)
+            {
+                throw new SecurityTokenValidationException
+                  ("Certificate was not issued by a trusted issuer");
+            }
+        }
+
     }
 }
